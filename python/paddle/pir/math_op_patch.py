a3e4ea3c96445526a0e61efdc4ac948486e5fa5c
zhouxin <zhou.xin2000@outlook.com>
2024-11-14 17:44:06 +0800

    fix typos

--- python/paddle/pir/math_op_patch.py
+++ python/paddle/pir/math_op_patch.py
@@ -1136,8 +1136,8 @@ def monkey_patch_value():
             _binary_creator_('__matmul__', paddle.tensor.matmul, False, None),
         ),
         (
-            '__matmul__',
-            _binary_creator_('__matmul__', paddle.tensor.matmul, False, None),
+            '__rmatmul__',
+            _binary_creator_('__rmatmul__', paddle.tensor.matmul, True, None),
         ),
         ('__neg__', _scalar_neg_),
         ('__abs__', _scalar_abs_),
